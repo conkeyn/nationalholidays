a46cf1db14b6f93bdf552f312caa74405e92a6f5
stevie.wong <qdlake@gmail.com>
2017-11-22 13:54:59 +0800

    fix: 修改CsvFileUtilsTest，改为ResourceUtils获取

--- backend/src/test/java/com/ykaoqin/kit/util/CsvFileUtilsTest.java
+++ backend/src/test/java/com/ykaoqin/kit/util/CsvFileUtilsTest.java
@@ -3,8 +3,12 @@ package com.ykaoqin.kit.util;
 import com.ykaoqin.kit.holiday.model.HolidayDay;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import static org.hamcrest.Matchers.greaterThan;
@@ -12,13 +16,20 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class CsvFileUtilsTest {
+    private static Logger logger = LoggerFactory.getLogger(CsvFileUtilsTest.class);
     private static String filePath = null;
 
     @BeforeClass
     public static void init() {
-        File file = new File(CsvFileUtils.class.getResource("/").getPath());
-        filePath = file.getParent() + File.separator + "resources";
-        System.out.println("filePath："+ filePath);
+        try {
+            File appFile = ResourceUtils.getFile("classpath:application.yml");
+            logger.info("app file path:"+ appFile.getPath());
+
+            filePath = appFile.getParent();
+            logger.info("csv files path:"+ filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
